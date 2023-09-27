93be16d3f5a7947e0c8f0c4a5c295641ce61fcdf
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-09-26 21:24:56 -0500

    Sin serialización

--- src/main/java/com/concesionario/ServerConcesionario.java
+++ src/main/java/com/concesionario/ServerConcesionario.java
@@ -1,13 +1,11 @@
 package com.concesionario;
 
-import java.io.EOFException;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import com.objeto.carro.Carro;
 
 public class ServerConcesionario {
     public static void main(String[] args) {
@@ -20,30 +18,21 @@ public class ServerConcesionario {
             ServerSocket server = new ServerSocket(puerto);
             Socket cliente = server.accept();
             PrintWriter out = new PrintWriter(cliente.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(cliente.getInputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
         ){
 
             System.out.println("Cliente conectado");
 
-            Carro carro = null;
+            String marcaCarro = null;
 
-            while(true){
-                carro = (Carro) in.readObject();
-
-                System.out.println(String.format("Recibido objeto: %s", carro.toString()));
-
-                out.println(String.format("Se ha recibido un carro de marca %s y color %s", carro.getMarca(), carro.getColor()));
-
+            while ((marcaCarro = in.readLine()) != null){
+                System.out.println(String.format("Recibido carro marca: %s", marcaCarro));
+                out.println(String.format("Se ha recibido un carro de marca %s y color %s", marcaCarro, "Verde"));
                 out.flush();
             }
 
-        }catch(EOFException eofe){
-            System.out.println("Cliente ha enviado objeto null (posiblemente)");
-            eofe.printStackTrace();
         }catch(IOException ioe){
             ioe.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
 
         System.out.println("Cerrando servidor...");
